67d946b16652b6e80d345e533eb0739d20a1c656
Opilane TTHK <158561962+mel2134@users.noreply.github.com>
2025-09-09 12:42:09 +0300

    students

--- Restful_api/students.js
+++ Restful_api/students.js
@@ -6,10 +6,30 @@ app.use(cors());
 app.use(express.json())
 var students = new Map()
 students.set(1,{name:"test",age:50,room:"aaaa123"})
+students.set(2,{name:"test",age:15,room:"aann246"})
+students.set(3,{name:"test",age:17,room:"bbbb123"})
 
+
+function AllStudents(){
+    var str = ""
+    for (var [key, element] of students) {
+        str += `<a href="/students/${key}">Student ${element.name}, Room: ${element.room}</a><br>`
+    }
+    return str
+}
 
 app.get('/',(req,res)=>{
-
+    return res.send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Widgets</title>
+</head>
+<body>
+    <a href="/widgets">All Widgets</a><br>` + AllStudents() +
+`</body>
+</html>`)
 })
 app.get('/students',(req,res)=>{
     var list = []
@@ -50,21 +70,25 @@ app.post('/students',(req,res)=>{
     if(!req.body.age){
         return res.status(400).send({error:"No student age specified!"})
     }
+    var age = parseInt(req.body.age)
+    if(!age){
+        return res.status(400).send({error:"Student age needs to be an integer"})
+    }
     if(!req.body.room){
         return res.status(400).send({error:"No student room specified!"})
     }
     var id = Math.max(...students.keys()) + 1
     var studentObject = {
         name:req.body.name,
-        age:req.body.age,
+        age:age,
         room:req.body.room
     }
     students.set(id,studentObject)
-    return res.status(201).send(students.get(id))
+    return res.status(201).location(`localhost:8080/students/${id}`).send(students.get(id))
 })
 
 app.get('/',(req,res)=>{
-    
+    return res.send()
 })
 app.listen(8080, ()=>{
     console.log("api up 8080")
